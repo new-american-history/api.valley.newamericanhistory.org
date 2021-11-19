7ecfa0f4417029972bf5d7aa9227f91b00636239
Emily Merricks <emily.merricks@journeygroup.com>
2021-11-19 14:40:21 -0500

    feat(migrations): add migration for free black registry

---
+++ database/migrations/2021_11_19_193821_create_free_black_registry_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFreeBlackRegistryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('free_black_registry', function (Blueprint $table) {
+            $table->id();
+            $table->string('county');
+            $table->string('city')->nullable();
+            $table->string('name')->nullable();
+            $table->string('number')->nullable();
+
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('free_black_registry');
+    }
+}
