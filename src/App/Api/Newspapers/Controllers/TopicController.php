02655d3a3cbb05e7886a66d29b0b1994d44965cf
Emily Merricks <emily.merricks@journeygroup.com>
2022-03-02 17:12:37 -0500

    feat(api): add index endpoint for newspaper topics

---
+++ src/App/Api/Newspapers/Controllers/TopicController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Api\Newspapers\Controllers;
+
+use Illuminate\Http\Request;
+use App\Api\Newspapers\Resources\TopicResource;
+use App\Api\Newspapers\Queries\TopicIndexQuery;
+
+class TopicController
+{
+    public function index(TopicIndexQuery $query)
+    {
+        return TopicResource::collection(
+            $query->paginate($request->perpage ?? 50)
+        );
+    }
+}
--- src/App/Api/Newspapers/Queries/EditionIndexQuery.php
+++ src/App/Api/Newspapers/Queries/EditionIndexQuery.php
@@ -4,7 +4,9 @@ namespace App\Api\Newspapers\Queries;
 
 use Illuminate\Http\Request;
 use Domain\Newspapers\Models\Edition;
+use Spatie\QueryBuilder\AllowedFilter;
 use Support\Queries\IndexQueryBuilder;
+use Illuminate\Database\Eloquent\Builder;
 
 class EditionIndexQuery extends IndexQueryBuilder
 {
@@ -14,7 +16,16 @@ class EditionIndexQuery extends IndexQueryBuilder
 
         parent::__construct($query, $request);
 
-        $this->allowedFilters($this->mapAllowedFilters(Edition::class));
+        $this->allowedFilters(
+            array_merge(
+                $this->mapAllowedFilters(Edition::class),
+                [
+                    AllowedFilter::callback('year', function (Builder $query, $value) {
+                        $query->whereYear('date', $value);
+                    })
+                ]
+            )
+        );
         $this->defaultSort('date');
     }
 }
