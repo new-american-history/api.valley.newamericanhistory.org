87cabb1ed36cddfa4e0eae11951f377ec40f850c
Joshua Bryant <joshb@journeygroup.com>
2026-05-12 10:09:20 -0400

    fix: include page (and especially page.edition) in StoryResource

--- src/App/Api/Newspapers/Resources/StoryResource.php
+++ src/App/Api/Newspapers/Resources/StoryResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Api\Newspapers\Resources;
 
+use App\Api\Newspapers\Resources\EditionResource;
 use App\Api\Newspapers\Resources\TopicResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -12,6 +13,14 @@ class StoryResource extends JsonResource
         $res = $this->resource->toArrayWithModernSpelling();
 
         $res += [
+            'page' => !empty($this->page)
+                ? [
+                    'number' => $this->page->number,
+                    'edition' => !empty($this->page->edition)
+                        ? new EditionResource($this->page->edition)
+                        : null,
+                ]
+                : null,
             'names' => !empty($this->names)
                 ? $this->names->map(function ($name) {
                     return new NameResource($name);
